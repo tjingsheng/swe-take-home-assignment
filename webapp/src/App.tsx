fc2ecf43431c3ddd143b526c9e18563c392ba1bb
tjingsheng <tanjs41200@gmail.com>
2025-03-06 02:11:21 +0800

    add salary table mvp

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,3 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SalaryTable } from "./components/SalaryTable.tsx";
+import { createTheme, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+const queryClient = new QueryClient();
+const theme = createTheme({});
+
 export function App() {
-  return <>Hello World</>;
+  return (
+    <MantineProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <SalaryTable />
+      </QueryClientProvider>
+    </MantineProvider>
+  );
 }
---
+++ webapp/src/components/SalaryTable.tsx
@@ -0,0 +1,41 @@
+import { Loader, Table } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export function SalaryTable() {
+  const { isLoading, data: persons } = useQuery({
+    queryKey: ["persons"],
+    queryFn: async () => {
+      const response = await axios.get("/api/users");
+      const responseData = response.data;
+      const persons = responseData.results as {
+        name: string;
+        salary: number;
+      }[];
+      return persons;
+    },
+  });
+
+  return isLoading ? (
+    <Loader />
+  ) : (
+    persons && (
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Name</Table.Th>
+            <Table.Th>Salary</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {persons.map((person) => (
+            <Table.Tr key={person.name}>
+              <Table.Td>{person.name}</Table.Td>
+              <Table.Td>{person.salary}</Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+    )
+  );
+}

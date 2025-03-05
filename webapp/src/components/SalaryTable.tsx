71668d82257430fc47b2fdcc5b8976502c25b5df
tjingsheng <tanjs41200@gmail.com>
2025-03-06 02:36:27 +0800

    add sort by name and salary

--- webapp/src/components/SalaryTable.tsx
+++ webapp/src/components/SalaryTable.tsx
@@ -1,12 +1,34 @@
-import { Loader, Table } from "@mantine/core";
+import {
+  ActionIcon,
+  Card,
+  Group,
+  Loader,
+  Stack,
+  Table,
+  Text,
+} from "@mantine/core";
+import { IconSortAscending } from "@tabler/icons-react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
+
+type SortKey = "DEFAULT" | "NAME" | "SALARY";
 
 export function SalaryTable() {
+  const [sort, setSort] = useState<SortKey>("DEFAULT");
+  const handleSort = (key: SortKey) => {
+    setSort((prev) => (prev === key ? "DEFAULT" : key));
+  };
+
   const { isLoading, data: persons } = useQuery({
-    queryKey: ["persons"],
+    queryKey: ["persons", sort],
     queryFn: async () => {
-      const response = await axios.get("/api/users");
+      const response = await axios.get("/api/users", {
+        params: {
+          sort: sort !== "DEFAULT" ? sort : undefined,
+        },
+      });
+
       const responseData = response.data;
       const persons = responseData.results as {
         name: string;
@@ -20,22 +42,50 @@ export function SalaryTable() {
     <Loader />
   ) : (
     persons && (
-      <Table>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>Name</Table.Th>
-            <Table.Th>Salary</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {persons.map((person) => (
-            <Table.Tr key={person.name}>
-              <Table.Td>{person.name}</Table.Td>
-              <Table.Td>${person.salary.toFixed(2)}</Table.Td>
+      <Stack>
+        <Card withBorder>
+          <Group></Group>
+        </Card>
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>
+                <Group gap="xs" align="center">
+                  <Text>Name</Text>
+                  <ActionIcon
+                    variant={sort === "NAME" ? "filled" : "subtle"}
+                    size="xs"
+                    onClick={() => handleSort("NAME")}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <IconSortAscending />
+                  </ActionIcon>
+                </Group>
+              </Table.Th>
+              <Table.Th>
+                <Group gap="xs" align="center">
+                  <Text>Salary</Text>
+                  <ActionIcon
+                    variant={sort === "SALARY" ? "filled" : "subtle"}
+                    size="xs"
+                    onClick={() => handleSort("SALARY")}
+                  >
+                    <IconSortAscending />
+                  </ActionIcon>
+                </Group>
+              </Table.Th>
             </Table.Tr>
-          ))}
-        </Table.Tbody>
-      </Table>
+          </Table.Thead>
+          <Table.Tbody>
+            {persons.map((person) => (
+              <Table.Tr key={person.name}>
+                <Table.Td>{person.name}</Table.Td>
+                <Table.Td>${person.salary.toFixed(2)}</Table.Td>
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+      </Stack>
     )
   );
 }
--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      // https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+    },
+  },
   server: {
     proxy: {
       "/api": {

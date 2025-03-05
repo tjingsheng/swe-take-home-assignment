614035adb538bf1aa88ee0521ebb9618746c9e62
tjingsheng <tanjs41200@gmail.com>
2025-03-05 18:48:24 +0800

    add seed scripts

---
+++ data/README.md
@@ -0,0 +1,3 @@
+# This is the "Database"
+
+Do not delete this folder. This is where you will find the seed data
---
+++ src/seed.ts
@@ -0,0 +1,48 @@
+import fs from "fs/promises";
+import path from "path";
+import { writeToPath } from "fast-csv";
+import { faker } from "@faker-js/faker";
+
+const DATA_FOLDER = "data";
+const CSV_FILE = "data.csv";
+const SEED_ROWS = 100000;
+const MIN_SALARY = 0;
+const MAX_SALARY = 10000;
+
+faker.seed(0);
+
+async function seed(): Promise<void> {
+  try {
+    const folderPath = path.join(process.cwd(), DATA_FOLDER);
+    const filePath = path.join(folderPath, CSV_FILE);
+
+    try {
+      await fs.access(folderPath);
+    } catch {
+      console.log(
+        `The data folder does not exist. Creating one at ${folderPath}`
+      );
+      await fs.mkdir(folderPath);
+    }
+
+    const seedData = Array.from({ length: SEED_ROWS }, () => ({
+      NAME: faker.person.firstName(),
+      SALARY: faker.finance.amount({
+        dec: 2,
+        min: MIN_SALARY,
+        max: MAX_SALARY,
+      }),
+    }));
+
+    writeToPath(filePath, seedData, { headers: true });
+
+    console.log(`CSV file seeded successfully at ${filePath}`);
+  } catch (error) {
+    console.error("Error writing CSV file:", error);
+  }
+}
+
+(async () => {
+  await seed();
+  console.log("🌱 Seeding completed 🌱");
+})();

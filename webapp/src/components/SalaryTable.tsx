dfde8755462cf4890ec1c34eff25b9e30afbc9df
tjingsheng <tanjs41200@gmail.com>
2025-03-06 03:03:21 +0800

    add min max

--- webapp/src/components/SalaryTable.tsx
+++ webapp/src/components/SalaryTable.tsx
@@ -1,6 +1,7 @@
 import {
   ActionIcon,
   Card,
+  Grid,
   Group,
   Loader,
   NumberInput,
@@ -19,19 +20,23 @@ export function SalaryTable() {
   const [sort, setSort] = useState<SortKey>("DEFAULT");
   const [offset, setOffset] = useState<number>(0);
   const [limit, setLimit] = useState<number>(0);
+  const [min, setMin] = useState<number>(0);
+  const [max, setMax] = useState<number>(4000);
 
   const handleSort = (key: SortKey) => {
     setSort((prev) => (prev === key ? "DEFAULT" : key));
   };
 
   const { isLoading, data: persons } = useQuery({
-    queryKey: ["persons", sort, offset, limit],
+    queryKey: ["persons", sort, offset, limit, min, max],
     queryFn: async () => {
       const response = await axios.get("/api/users", {
         params: {
           sort: sort !== "DEFAULT" ? sort : undefined,
           offset: Math.max(offset, 0),
           limit: Math.max(limit, 0),
+          min,
+          max,
         },
       });
 
@@ -50,22 +55,46 @@ export function SalaryTable() {
     persons && (
       <Stack>
         <Card withBorder>
-          <Group>
-            <NumberInput
-              label="Offset"
-              value={offset}
-              min={0}
-              description="Offset from the beginning"
-              onChange={(value) => setOffset(parseInt(String(value), 10))}
-            />
-            <NumberInput
-              label="Limit"
-              value={limit}
-              min={0}
-              description="0 means no limit"
-              onChange={(value) => setLimit(parseInt(String(value), 10))}
-            />
-          </Group>
+          <Grid>
+            <Grid.Col span={{ sm: 12, md: 6 }}>
+              <NumberInput
+                label="Offset"
+                value={offset}
+                min={0}
+                description="Offset from the beginning"
+                onChange={(value) => setOffset(parseInt(String(value), 10))}
+              />
+            </Grid.Col>
+            <Grid.Col span={{ sm: 12, md: 6 }}>
+              <NumberInput
+                label="Limit"
+                value={limit}
+                min={0}
+                description="0 means no limit"
+                onChange={(value) => setLimit(parseInt(String(value), 10))}
+              />
+            </Grid.Col>
+            <Grid.Col span={{ sm: 12, md: 6 }}>
+              <NumberInput
+                label="Min"
+                value={min}
+                min={0}
+                step={100}
+                description="Should be less than Max"
+                onChange={(value) => setMin(parseInt(String(value), 10))}
+              />
+            </Grid.Col>
+            <Grid.Col span={{ sm: 12, md: 6 }}>
+              <NumberInput
+                label="Max"
+                value={max}
+                min={0}
+                step={100}
+                description="Should be greater than Min"
+                onChange={(value) => setMax(parseInt(String(value), 10))}
+              />
+            </Grid.Col>
+          </Grid>
         </Card>
         <Table>
           <Table.Thead>
